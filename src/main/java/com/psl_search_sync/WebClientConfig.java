93e69993ff1d9e6c41c121ef1bb16608b4c004ce
Aditya Pandit <aditya.pandit@purplestylelabs.com>
2026-01-17 19:11:25 +0530

    added comf

--- src/main/java/com/psl_search_sync/WebClientConfig.java
+++ src/main/java/com/psl_search_sync/WebClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -11,8 +12,15 @@ public class WebClientConfig {
 
     @Bean
     public WebClient embeddingWebClient() {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer ->
+                        configurer.defaultCodecs()
+                                .maxInMemorySize(10 * 1024 * 10024) // 10 MB
+                )
+                .build();
         return WebClient.builder()
                 .baseUrl("http://141.94.137.133:6645")
+                .exchangeStrategies(strategies)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
